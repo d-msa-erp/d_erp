5f9a037cd25ffccb3fb5edc7e59def8f496cb8d2
Minsub00 <lk11456@naver.com>
2025-05-31 02:27:03 +0900

    Fix: 주문, 발주 오류 수정#55

--- src/main/java/kr/co/d_erp/dtos/OrderResponseDto.java
+++ src/main/java/kr/co/d_erp/dtos/OrderResponseDto.java
@@ -12,5 +12,7 @@ import lombok.NoArgsConstructor;
 public class OrderResponseDto {
 	private Long orderIdx;
     private String orderCode;
+    private boolean productShortage;   // 재고 부족 여부
+    private boolean materialShortage;  // 자재 부족 여부
     private List<String> warnings;
 }
