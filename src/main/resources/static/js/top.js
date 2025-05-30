7d58570b77a10c7d5bce91e11a2b570a5f025b62
SeonikKim <tjsdlrdl122@naver.com>
2025-05-30 11:28:21 +0900

    Design : 반응형 적용 ... #81

--- src/main/resources/static/js/top.js
+++ src/main/resources/static/js/top.js
@@ -1,25 +1,48 @@
 
 function toggleMenu(event, el) {
-      event.stopPropagation();//버블링 방지
+	event.stopPropagation();//버블링 방지
 
-      // 이미 active면 해제
-      if (el.classList.contains('active')) {
-        el.classList.remove('active');
-        return;
-      }
+	// 이미 active면 해제
+	if (el.classList.contains('active')) {
+		el.classList.remove('active');
+		return;
+	}
 
-      // 다른 메뉴 닫기
-      document.querySelectorAll('.menu-item').forEach(item => {
-        item.classList.remove('active');
-      });
+	// 다른 메뉴 닫기
+	document.querySelectorAll('.menu-item').forEach(item => {
+		item.classList.remove('active');
+	});
 
-      // 현재 메뉴만 활성화
-      el.classList.add('active');
-    }
+	// 현재 메뉴만 활성화
+	el.classList.add('active');
+}
 
-    // 바깥 누르면 모두 닫기
-    document.addEventListener('click', () => {
-      document.querySelectorAll('.menu-item').forEach(item => {
-        item.classList.remove('active');
-      });
-    });
+// 바깥 누르면 모두 닫기
+document.addEventListener('click', () => {
+	document.querySelectorAll('.menu-item').forEach(item => {
+		item.classList.remove('active');
+	});
+});
+
+
+//햄버거 핸들링용
+document.addEventListener('DOMContentLoaded', function() {
+
+	const button = document.getElementById('hamburger-btn');
+	const nav = document.getElementById('main-nav');
+
+
+	if (button && nav) {
+		button.addEventListener('click', function() {
+
+			const currentDisplay = window.getComputedStyle(nav).display;
+			if (currentDisplay === 'none') {
+				nav.style.display = 'flex';
+				nav.style.flexDirection = 'column';
+			} else {
+				nav.style.display = 'none';
+			}
+		});
+
+	}
+});
